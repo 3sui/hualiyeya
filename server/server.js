742de35e0863427307a70e407d66e7bdd11ef909
wuqi <178289023@qq.com>
2020-06-01 10:13:17 +0800

    初始化项目

---
+++ server/server.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-08 09:03:27
+ * @LastEditTime: 2020-05-27 14:57:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \server\server.js
+ */
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(express.json())
+app.use(cors())
+
+//开放uploads文件夹
+app.use('/uploads', express.static(__dirname + '/uploads'))
+//连接服务器
+require('./mysql/mysql.js')(app)
+require('./route/admin/productProfile/index')(app)
+app.get('/abc', (res, req) => {
+    console.log(123);
+
+    req.send('123')
+})
+
+
+app.listen(9999, (err) => {
+    if (err) {
+        console.log(err);
+
+    } else {
+        console.log('服务器已连接,端口号9999');
+
+    }
+})
+
+// app.listen(8890, (err) => {
+//     if (err) {
+//         console.log(err);
+
+//     } else {
+//         console.log('服务器已连接,端口号8890');
+
+//     }
+// })
